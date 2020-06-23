a926d724cd5b7d3c948364e8db50dd19b66a6ab2
noael-lab <nopperilab@gmail.com>
2020-06-23 15:01:24 +0900

    とりあえず

---
+++ Assets/Script/AnimController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+    [SerializeField]
+    Animator Animator;
+    [SerializeField]
+    int animNum;
+
+    [SerializeField]
+    float waitTime;
+    float count;
+
+    void FixedUpdate()
+    {
+        count += Time.deltaTime;
+        if (count > waitTime)
+        {
+            ChangeAnim();
+        }
+    }
+    public void ChangeAnim()
+    {
+        Animator.SetInteger("KeyNumber", Random.Range(0, animNum));
+        count = 0;
+    }
+}
---
+++ Assets/Script/ClockController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class ClockController : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI TimeText;
+
+    [SerializeField]
+    TextMeshProUGUI DateText;
+
+    string[] week = new string[7]
+    {
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sut"
+    };
+
+    void FixedUpdate()
+    {
+        if (TimeText.text != DateTime.Now.ToShortTimeString())
+        {
+            TimeText.text = DateTime.Now.ToShortTimeString();
+        }
+        if (DateText.text != DateTime.Now.ToString("yyyy/MM/dd/") + week[(int)DateTime.Now.DayOfWeek])
+        {
+            DateText.text = DateTime.Now.ToString("yyyy/MM/dd/") + week[(int)DateTime.Now.DayOfWeek];
+        }
+    }
+    public void CloseClock()
+    {
+        Application.Quit();
+    }
+}
